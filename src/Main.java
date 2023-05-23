08e2dc8898ed454a3bb85de183f74e2d27b7722b
BongWonHee <leeban1004@gmail.com>
2023-05-23 15:57:43 +0900

    오전과업

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         String name = "WonHee Bong";
         System.out.println(name);
+        name = "Bong";
+        System.out.println();
         // return 0;
 
     }
---
+++ src/MakingGamebyOneHandGame.java
@@ -0,0 +1,14 @@
+public class MakingGamebyOneHandGame {
+    public static void main(String[] args) {
+        String A = "1";
+        String B = "2";
+        String C = "3";
+
+        A = B ;
+        C = A ;
+        B = C ;
+        
+        //return 0;
+    }
+    
+}
