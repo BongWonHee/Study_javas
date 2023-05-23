2efc86cf571211183a774f4eccec07a81acdbf35
BongWonHee <leeban1004@gmail.com>
2023-05-23 17:29:50 +0900

    05.23중간정산

---
+++ src/Ifs.java
@@ -0,0 +1,18 @@
+
+public class Ifs {
+    public static void main(String[] args) {
+        // if (condition) {
+        // // block of code to be executed if the condition is true
+        // } else{
+
+        // }
+        int first = 20;
+        int second = 18;
+        if (first > second) {
+            System.out.println(first);
+        }
+        System.out.println("hello My World!!");
+        // return 0;
+    }
+
+}
