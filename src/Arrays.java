216ae80dbb2f4898185031db7dfc511a7712e844
BongWonHee <leeban1004@gmail.com>
2023-05-24 18:21:17 +0900

    마지막 과제

---
+++ src/Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for(int list = 0; list <4 ;list = list + 1)
+        System.out.println(cars[list]);
+
+        
+        //return 0;
+    }
+}
