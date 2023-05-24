04ba56d219a15590188eb73b379f33f280df3c2e
BongWonHee <leeban1004@gmail.com>
2023-05-24 15:27:35 +0900

    변경

--- src/LoopWithMark.java
+++ src/LoopWithMark.java
@@ -1,7 +1,7 @@
 public class LoopWithMark {
     public static void main(String[] args) {
         for (int i = 1; i < 5; i = i + 1) {
-            System.out.println("for" + i);
+            System.out.println("for " +  i);
 
         }
         System.out.println("end");
