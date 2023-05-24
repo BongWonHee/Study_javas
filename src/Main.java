07538f323598b5eb1d7b2d76469147c920eb78c6
BongWonHee <leeban1004@gmail.com>
2023-05-24 15:23:15 +0900

    중간과제

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         
-        Scanner taja = new Scanner(System.in);
-     
-        int a;
-        int b;
-
-        a = taja.nextInt();
-        b = taja.nextInt();
-        System.out.println(a + b);
+        
         
         // return 0;
 
