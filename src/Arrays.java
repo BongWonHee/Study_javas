f855a09da4bc890257cc7b2966859ec55e270f8f
BongWonHee <leeban1004@gmail.com>
2023-05-25 18:21:00 +0900

    여기까지

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,10 +1,30 @@
 public class Arrays {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        for(int list = 0; list <4 ;list = list + 1)
-        System.out.println(cars[list]);
+        // String[] cars = { "Volvo", "BMW", "Ford", "Mazda", "KIA" };
+        // System.out.println(cars.length);
+        // for(int num = 0; num < cars.length ; num = num + 1){
+        // System.out.print(cars[num] + ", ");
 
-        
-        //return 0;
+        String[][] numbers = {
+            { "10", "20", "30", "40" }, 
+            {"50", "60", "70"}
+        };
+        for(int num = 0; num < numbers.length; num = num + 1){
+            for(int num1 = 0  ; num1 < numbers[0].length; num1 = num1 + 1 ){
+                // System.out.print(numbers[0].length);
+                for(int num2 = 0; num2 < numbers[num1].length; num2 = num2 + 1 ){
+                    System.out.println(numbers[num1][num2] + ", ");
+
+                }
+                System.out.println();
+
+            }
+            // System.out.print(numbers[]);
+        }
+
+        System.out.println();
+
+        // return 0;
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
-        
-        
-        // return 0;
+
+        Scanner taja = new Scanner(System.in);
+
+        int a;
+
+        a = taja.nextInt();
+
+        if (a % 4 == 0 || a % 100 != 0 || a % 400 == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
 
     }
+
+    // return 0;
+
 }
-
-
