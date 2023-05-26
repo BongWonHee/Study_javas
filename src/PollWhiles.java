5f67e750f55f820205a4a084f62ce4f412f5dfc8
BongWonHee <leeban1004@gmail.com>
2023-05-26 14:30:01 +0900

    오전과제 최종버전

--- src/PollWhiles.java
+++ src/PollWhiles.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class PollWhiles {
     public static void main(String[] args) {
-        //해당 입력키에 따라 안내문이 출력합니다.
+        // 해당 입력키에 따라 안내문이 출력합니다.
         try {
 
             Scanner myOjt = new Scanner(System.in);
@@ -13,27 +13,18 @@ public class PollWhiles {
             while (true) {
                 System.out.println("선택에 따라 안내글을 보여드립니다.");
                 taja = myOjt.nextLine();
-                if (taja.equals("E")) { //입력키 문자타입을 입력 받을때는.equals 를 사용자!!
+                if (taja.equals("E") || taja.equals("Exit")) { // 입력키 문자타입을 입력 받을때는.equals 를 사용함
                     System.out.println("-----설문 종료-----");
-                } else if (taja.equals("Exit")) {
-                    System.out.println("-----설문 종료-----");
-
-                } else if (taja.equals("P")) {
-                    System.out.println("-----설문 통계-----");
-
-                } else if (taja.equals("Poll")) {
-                    System.out.println("-----설문 통계-----");
-
-                } else if (taja.equals("S")) {
-                    System.out.println("-----설문 시작-----");
-
-                } else if (taja.equals("Statistic")) {
-                    System.out.println("-----설문 시작-----");
-
-                } else {
-                    System.out.println("설문에 참여해주셔서 감사합니다.");
+                    System.out.println("설문에 참여하여 주셔서 감사합니다.");
                     break;
-                }
+                    // ((taja.equals("E") || taja.equals("Exit"))
+                } else if (taja.equals("P") || taja.equals("Poll")) {
+                    System.out.println("-----설문 통계-----");
+                    
+                }  else if (taja.equals("S") || taja.equals("Statistic")) {
+                    System.out.println("-----설문 시작-----");
+
+                }  
             }
 
         } catch (Exception e) {
