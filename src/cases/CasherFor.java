ace1567013b1d8013851510529f4aa70cd484641
BongWonHee <leeban1004@gmail.com>
2023-05-27 01:37:49 +0900

    과제끝

--- src/cases/CasherFor.java
+++ src/cases/CasherFor.java
@@ -1,16 +1,51 @@
-// 장바구니에 5개에 입력된 숫자가 0 이면 계산을 하고 , 100을 초과하면  값은 0을 가지고 계산을 시작해라
-// 장바구니엔 5가지만 넣을 수 있습니다.
-// - 입력 중 0원이면 입력되었던 값 합산
-// -100원 이하로 입력 제한 , 이상 시 0원적용
+package cases;
 
-// 장바구니에 5개 넣기.
-// ---계산시작 ----
+import java.util.Scanner;
 
+public class CasherFor {
+    // 장바구니에 5개의 물품 가격을 입력하고 가격이 100원이 넘으면 초과 메시지를 생성하고, 0원으로 계산한다. 0을 입력하면 최종 가격을
+    // 출력해준다.
+    public static void main(String[] args) {
 
-// 담은 총합
-// 합계:45
-// 계산 종료
-// int Price_1 = 40;
-// int Price_2 = 140;
-// int Price_3 = 5;
-// int Price_4 = 0;
+        Scanner myOjt = new Scanner(System.in);
+
+        int[] price = new int[5];
+
+        System.out.println("--- 계산 시작---");
+
+        int money;
+
+        for (int first = 0; first < price.length; first = first + 1) {
+            money = myOjt.nextInt();
+
+            if (money > 0 && money <= 100) {
+                System.out.println("가격은" + money + "원 입니다.");
+                // 가격이 0 이상이고 100보다 작거나 같으면 설정
+                price[first] = money;
+            } else if (money > 100) {
+                System.out.println("가격 100원을 초과 하였습니다.");
+                break;
+
+                // 가격이 0 이거나 100을 초과하면 종료
+
+            } else if (money == 0) {
+                break;
+
+            }
+            money = 0;
+
+        }
+        System.out.println("--- 계산 종료 합니다.---");
+        // 종료 후 총 입력된 수의 합을 출력한다.
+        // 합을 저장할 변수 초기화
+        System.out.println(price[0] + price[1] + price[2] + price[3] + price[4]);
+        // 배열의 각 항을 순회하며 합을 구함
+
+        // int sum = 0;
+        // for (int i = 0; i < price.length; i++) {
+        // sum += price[i];
+        // }System.out.println("합계 : " + price);
+
+    }
+
+}
