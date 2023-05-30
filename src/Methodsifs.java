cec28880c8e540b1382fa8465c78af555b13f6d4
BongWonHee <leeban1004@gmail.com>
2023-05-30 18:31:44 +0900

    진짜끝임

--- src/Methodsifs.java
+++ src/Methodsifs.java
@@ -5,7 +5,6 @@ public class Methodsifs {
 
     public String alltime(int times) {
         String str = "";
-
         try {
             if (times < 12) {
                 str = "오전입니다.";
@@ -25,13 +24,17 @@ public class Methodsifs {
     public static void main(String[] args) {
 
         try {
+            
             int times = 0; // time 0 ~ 24까지 사용함다.
+            while(times < 25){
+            
             Scanner my = new Scanner(System.in);
             times = my.nextInt();
 
             Methodsifs methodsifs = new Methodsifs();
             String Resulttime = methodsifs.alltime(times);
             System.out.println(Resulttime);
+            }
 
         } catch (Exception e) {
             // TODO: handle exception
