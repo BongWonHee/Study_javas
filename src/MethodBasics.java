57ca98f6a17ec0833df9a6680a224ee88fe9cdf0
BongWonHee <leeban1004@gmail.com>
2023-05-30 14:16:27 +0900

    오전과제 완료

--- src/MethodBasics.java
+++ src/MethodBasics.java
@@ -6,30 +6,33 @@ public class MethodBasics {
     //     body
     // }
 // x에 + 1
-    // public int F(int x) //return_data_type을 return의 변수 타입을 동일시 한다. 
-    // {
-    //     int result = x + 1;
+    public int F(int x) //return_data_type을 return의 변수 타입을 동일시 한다. 
+    {
+        int result = x + 1;
         
-    //     return result; //출력하는 변수를 입력해준다.
-    // }
+        return result; //출력하는 변수를 입력해준다.
+    }
 
     // y의 메소드 과제
-    public int New(int y){
-        int yresult = y + 1;
-        return yresult;
+    public int F(int x, int y){
+        int result = x + 1;
+        result = y + 1;
+        return result;
     }
     public static void main(String[] args) {
         try {
             int y = 0;
             int x = 0;
-            int z = 0;
+            int New = 0;
             // y = x + 1; //변수에 +1
             // MethodBasics metehodBasics = new MethodBasics();
             // y = metehodBasics.F(x);
             // y = F(x);
             // y = y + 1; //변수에 +1
-            MethodBasics yrecall = new MethodBasics();
-            z = yrecall.New(y); 
+            MethodBasics methodBasics = new MethodBasics();
+            y = methodBasics.F(x);
+            New = methodBasics.F(y);
+             
         } catch (Exception e) {
             // TODO: handle exception
         }
