fef057f01e5e3f5c2439142dfd3b1a5f292db6ca
BongWonHee <leeban1004@gmail.com>
2023-05-31 11:37:21 +0900

    add code

---
+++ src/GitConflicts.java
@@ -0,0 +1,10 @@
+public class GitConflicts {
+    public static void main(String[] args) {
+        try {
+            int first = 2;
+            first = first + 1 ;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }//return 0 ;
+}
