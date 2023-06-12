56946c3c91a1a5bcb695bc16c9a69b9f7e319e39
BongWonHee <leeban1004@gmail.com>
2023-06-12 12:25:23 +0900

    add

--- src/ClassHasMaps.java
+++ src/ClassHasMaps.java
@@ -10,7 +10,7 @@ public class ClassHasMaps {
             hashMap.get("third");// Integer@52 "3" 키값으로 불러옴
             hashMap.size(); // 3 사이즈 확인
             hashMap.put("second", 5); // Integer@48 "2" 해당 키값을 넣고 변경할 value 입력.
-            hashMap.remove("second");// Integer@60 "5" 키우고싶은 키값을 삭제 한다.
+            hashMap.remove("second");// Integer@60 "5" 지우고싶은 키값을 삭제 한다.
             hashMap.keySet();// HashMap$KeySet@66 size=2 0:"third"1:"frist" key값만 전체 확인할때.
             hashMap.values();// HashMap$Values@70 size=2 0:Integer@52 1:Integer@29 value값만 전체 확인할때.
             hashMap.clear(); // HashMap 을 비울때 사용
--- src/ConnectDBs.java
+++ src/ConnectDBs.java
@@ -7,7 +7,7 @@ public class ConnectDBs {
         try {
             // - MySQL workbench 실행 : JDBC
             // - User/password와 접속 IP:port 접속
-            String url = "jdbc:mysql://localhost:3306/db_cars";
+            String url = "jdbc:mysql://127.0.0.1:3306/db_cars";
             String user = "root";
             String password = "!yojulab*";
 
@@ -23,10 +23,37 @@ public class ConnectDBs {
                         resultSet.getString("COMPANY"));
 
             }
+            query = "select count(*) as CNT from factorys;";
+            resultSet = statement.executeQuery(query); // select문에서 table 형태의 결과값 출력명령어
+            int totalCount = 0;
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("CNT"));
+                totalCount = resultSet.getInt("CNT");
 
+            }
+
+            // insert into factorys
+            // (COMPANY_ID, COMPANY)
+            // value
+            // ('CAR-01', 'AUDI');
+
+            String companyid = "CAR-01";
+            String company = "AUDI";
+            int count = 0;
+
+            query = "insert into factorys " + "(COMPANY_ID, COMPANY) " + "value " + "('" + companyid + "', '" + company
+                    + "') ";
+            count = statement.executeUpdate(query); // executeUpdate 는 insert, update, delete 를 사용할때 씀
+
+            query = " UPDATE factorys " + "SET COMPANY = '페라리' " + "WHERE COMPANY_ID = '"+companyid+"' ";
+            count = statement.executeUpdate(query); // CAR-01 의 AUDI를 페라리로 변경
+          
+            query = "delete FROM factorys " + "WHERE COMPANY_ID = '"+companyid+"'";
+            count = statement.executeUpdate(query); // CAR-01 삭제
             System.out.println();
         } catch (Exception e) {
             // TODO: handle exception
+            System.out.println(e.getMessage());
         }
         System.out.println();
         // retrun 0 ;;
