a6dbc2a081fae0e09aea755eafc78cbfa437e39b
BongWonHee <leeban1004@gmail.com>
2023-06-15 16:58:13 +0900

    add

--- src/SurveysApp.java
+++ src/SurveysApp.java
@@ -16,22 +16,31 @@ public class SurveysApp {
             String url = "jdbc:mysql://127.0.0.1:3306/db_survey";
             String user = "root";
             String password = "!yojulab*";
-            String query = "";
+            String queryA = "";
             Connection connection = DriverManager.getConnection(url, user, password); // network 자원사용
             System.out.println("DB연결 성공\n");
 
             // - query Edit
             Statement statement = connection.createStatement(); // DB자원
-            FactoryDMLs factoryDMLs = new FactoryDMLs();
-            ResultSet resultSet = factoryDMLs.selectStatements(statement, query); // select문에서 table 형태의 결과값 출력명령어
+
+            System.out.println("--- 통계 ---");
+            // -- 총 설문자 : 3명
+
+            queryA = "select count(*) CNT\n" + //
+                    "from\n" + //
+                    "(SELECT \n" + //
+                    "RESPONDENTS_ID, COUNT(RESPONDENTS_ID) AS CNT\n" + //
+                    "FROM\n" + //
+                    "statistics\n" + //
+                    "GROUP BY RESPONDENTS_ID) as T_STATIC ";
+            ResultSet resultSet = statement.executeQuery(queryA); // select문에서 table 형태의 결과값 출력명령어
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("COMPANY_ID") +
-                        resultSet.getString("COMPANY"));
+                System.out.println("--- 총 질문자 : " + resultSet.getString("CNT"));
 
             }
 
         } catch (Exception e) {
-            
+
             System.out.println(e.getMessage());
         }
 
