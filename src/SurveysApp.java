6c523114ecce7e08d4a3332671fc2052e4a4cdfa
BongWonHee <leeban1004@gmail.com>
2023-06-15 18:19:51 +0900

    add

--- src/SurveysApp.java
+++ src/SurveysApp.java
@@ -4,12 +4,8 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.HashMap;
 
-<<<<<<< HEAD
 import Surveys.Statistics;
-=======
-import com.mysql.cj.jdbc.interceptors.ResultSetScannerInterceptor;
 
->>>>>>> 66ae7a39dd2b6a818ee7d08a7fab79fd87ddc376
 import cars.FactoryDMLs;
 
 import java.util.ArrayList;
@@ -29,10 +25,53 @@ public class SurveysApp {
             // - query Edit
             Statement statement = connection.createStatement(); // DB자원
 
+            System.out.println("--- 설문자 가능 명단 ---");
+
+            // - 설문자 가능 명단(가입 완료)
+            // 1. 홍길동, 2.장길산, 3.신사임당, ...
+            queryA = "select *\n" + //
+                    "From respondents";
+
+            ResultSet resultSet = statement.executeQuery(queryA);
+            int number = 1;
+            while (resultSet.next()) {
+                System.out.print(number + "." + resultSet.getString("RESPONDENTS") + "  ");
+                number = number + 1;
+            }
+            System.out.println();
+
+            // -- 설문 시작
+            // -------- 참조 : poll contents example -------------
+            // -- 1. 교수는 수업 전 강의 목표를 명확히 제시하였습니까?
+            // -- (1)전혀 아니다. (2)아니다. (3)그렇다. (4)매우그렇다.
+            // -- 2. 강의의 내용은 체계적이고 성의있게 구성되었는가?
+            // -- (1)전혀 아니다. (2)아니다. (3)그렇다. (4)매우그렇다.
+            System.out.println("--- 설문 시작 ---");
+            queryA = "select *\n" +
+                    "From questions";
+
+            resultSet = statement.executeQuery(queryA);
+            Statement statement_second = connection.createStatement();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("questions"));
+                queryA = "select *\n" + //
+                        "From choice";
+
+                ResultSet resultSet_second = statement_second.executeQuery(queryA);
+                number = 1;
+                while (resultSet_second.next()) {
+                    System.out.print(number + "." + resultSet_second.getString("choice")+ " ");
+                    number = number + 1;
+                    
+                }
+                System.out.println();
+            }
+
+            // -- 총 설문자 : 3명
+            // select문에서 table 형태의 결과값 출력명령어
             Statistics statistics = new Statistics();
             statistics.getRespondents(statement);
 
-           
             // SELECT COUNT(*) CNT
             // FROM (SELECT RESPONDENTS_ID, COUNT(*) AS CNT
             // FROM statistics
@@ -48,7 +87,6 @@ public class SurveysApp {
             ResultSet reusltSet = statement.executeQuery(queryB);
             while (reusltSet.next()) {
                 System.out.println(reusltSet.getString("CNT"));
-                        
 
             }
 
