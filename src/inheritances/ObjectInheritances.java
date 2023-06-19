602f04b426619bca4814fa2c2535241b7d68fcf2
SSSanga <ksa901125@gmail.com>
2023-06-19 13:18:18 +0900

    B push

---
+++ src/inheritances/ObjectInheritances.java
@@ -0,0 +1,13 @@
+package inheritances;
+
+public class ObjectInheritances extends Object{
+    public int FirstMethods(String first){
+        try {
+            System.out.println("FirstMethods");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());//오류 잡아줌
+            // TODO: handle exception
+        }
+        return 0;
+    }
+}
---
+++ src/vehicles/Bicycle.java
@@ -0,0 +1,22 @@
+package vehicles;
+
+import java.util.HashMap;
+
+public class Bicycle {
+    int price;
+    String color;
+    HashMap<String, Object> spec;
+    public Bicycle(){
+    System.out.println("Bicycle - param empty");
+    }
+    public Bicycle(int price, String color){
+        this.price = price;
+        this.color = color;
+    }
+
+    public String toString(){
+        String str = "price :"+ this.price + ", color :"+ this.color;
+        return str;
+    } 
+    // this 2개를 같이 출력하게끔. 
+}
